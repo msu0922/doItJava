a9afe40eab07d64d09bb735cebd0299fbca0c7f5
SeWoongMin <msu9909@naver.com>
2022-03-14 22:14:23 +0900

    22.03.14

---
+++ src/Interface/Calc.java
@@ -0,0 +1,13 @@
+package Interface;
+
+public interface Calc {
+
+    double PI = 3.14; // = public static final
+    int ERROR = -999999999;
+
+    int add(int num1, int num2);
+    int subtract(int num1, int num2);
+    int multiply(int num1, int num2);
+    int divide(int num1, int num2);
+
+}
---
+++ src/Interface/CalculatorTest.java
@@ -0,0 +1,15 @@
+package Interface;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 2;
+
+        Calc calc = new CompleteCalculator();
+
+        System.out.println(calc.add(num1, num2));
+
+
+    }
+}
